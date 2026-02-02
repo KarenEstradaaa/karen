ad7871cecde88c5b1e734d05817f24145666aa1e
Karen <karen_estrada@berkeley.edu>
2026-02-02 11:30:19 -0800

    Saving homework1/ folder

---
+++ homework1/homework1.py
@@ -0,0 +1,43 @@
+File: homework1. py
+# --- Variables and Data Typles ---
+a=10
+print(a)
+print(type(a)) # a is an integer, a whole number with no decimals
+b=1.5
+print(b)
+print(type(b)) # b is float, a fractional number or a number with decimals
+c=3j
+print(c)
+print(type(c)) # c is complex, represents numbers with both real and imaginary parts (complex numbers)
+d="hello"
+print(d)
+print(type(d)) # d is strings, represents words or text
+e= [1, 2, 3]
+print(e)
+print(type(e)) # e is a list, which holds a collection of items
+f= {"Karen", "mangos"}
+print(f) # f is a set, used to store multiple items in a single variable
+print(type(f))
+g = (1, 2)
+print (g)
+print(type(g)) # g is a tuple, tuples are used to store multiple items in a single variable
+h=["apple", "banana", "straberry"]
+print(h)
+print(type(h)) #h is a list
+i= True
+print(i)
+print(type(i)) # i is a bool, it represents two truth values
+j= None
+print(j)
+print(type(j)) # j is a NoneType, 
+k= [True, "blue", 12]
+print(k)
+print(type(k)) # k is a list
+l = str(14)
+print(l)
+print(type(l)) # l is a strings
+m= 1e4
+print(m)
+print(type(m)) # m is a float
+
+# 1. 
